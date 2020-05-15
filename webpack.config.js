85e8ecc37bd6b48a446870e40216fdb4448cf8db
Erick Sosa Garcia <ericksosagarcias@gmail.com>
2020-05-15 18:39:55 -0400

    version to testing new features

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+
+module.exports = {
+  entry: "./dev/js/main2.js",
+  watch: false,
+  watchOptions: {
+    ignored: ['node_modules/**', 'dev/jsx/**', 'router/**', 'src/**']
+  },
+  mode:  true ? "production" : "development",
+  output: {
+    path: path.join(__dirname, "dev/"),
+    filename: "bundle.js",
+  },
+}
